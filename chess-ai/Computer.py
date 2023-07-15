80014da575a205bd7c4f9671ee2f54d433a87c5d
Vitus Nyvang Legarth <110633417+Vinyle02@users.noreply.github.com>
2023-07-15 16:54:14 +0200

    Kan spiller, der er en bug

--- chess-ai/Computer.py
+++ chess-ai/Computer.py
@@ -27,8 +27,9 @@ def minimax(board, depth,max_player, alpha, beta, save_move=False,):
                     moves = piece.get_moves(board)
                     if moves:
                         for move in moves:
+
                             board.make_move(piece, move[0], move[1], True)
-                            mini = minimax(board, depth - 1, False, -10000, 10000)
+                            mini = minimax(board, depth - 1, False, alpha, beta)
 
                             enemy_data = mini[0][2]
                             evaluation = 0
@@ -38,8 +39,8 @@ def minimax(board, depth,max_player, alpha, beta, save_move=False,):
                                 evaluation += board.evaluate(True)
                             evaluation += enemy_data
 
-
                             if evaluation > max_eval:
+
                                 data.clear()
                                 data.append([piece,move,evaluation,mini[0][0],mini[0][1]])
 
@@ -51,6 +52,7 @@ def minimax(board, depth,max_player, alpha, beta, save_move=False,):
                             alpha = max(alpha, max_eval)
                             if beta <= alpha:
                                 break
+        return data
     else:
         min_eval = 9000000000
 
@@ -88,8 +90,84 @@ def minimax(board, depth,max_player, alpha, beta, save_move=False,):
                             beta = min(beta, min_eval)
                             if beta <= alpha:
                                 break
-    return data
+        return data
 
+def get_sorted_moves(board, maximising):
+    moves = []
+    for i in range(8):
+        for j in range(8):
+            piece = board[i][j]
+            if isinstance(piece, ChessPiece) and ((maximising and piece.color != board.get_player_color()) or
+                                                  (not maximising and piece.color == board.get_player_color())):
+                for move in piece.get_moves(board):
+                    board.make_move(piece, move[0], move[1], True)  # Make the move
+                    score = board.evaluate(True) if board.get_player_color() == "white" else board.evaluate(False)
+                    moves.append((score, piece, move))
+                    board.unmake_move(piece)  # Undo the move
+
+    # Sort the moves in descending order of score if maximising player, and ascending if minimising
+    moves.sort(key=lambda x: x[0], reverse=maximising)
+    return moves
+
+def minimax2(board, depth,max_player, alpha, beta, save_move=False,):
+    global positions_evaluated
+    if depth == 0:
+        return [[0,0,0]]
+    data = []
+    sorted_moves = get_sorted_moves(board, max_player)
+
+    if max_player:
+        max_eval = -900000000000
+
+        for score, piece, move in sorted_moves:
+            board.make_move(piece, move[0], move[1], True)
+            mini = minimax2(board, depth - 1, False, alpha, beta)
+            enemy_data = mini[0][2]
+            evaluation = 0
+            if board.get_player_color() == "white":
+                evaluation += board.evaluate(False)
+            else:
+                evaluation += board.evaluate(True)
+            evaluation += enemy_data
+            if evaluation > max_eval:
+
+                data.clear()
+                data.append([piece,move,evaluation,mini[0][0],mini[0][1]])
+
+            elif evaluation == max_eval:
+                data.append([piece,move,evaluation,mini[0][0],mini[0][1]])
+            max_eval = max(max_eval, evaluation)
+            positions_evaluated += 1
+            board.unmake_move(piece)
+            alpha = max(alpha, max_eval)
+            if beta <= alpha:
+                break
+    else:
+        min_eval = 9000000000
+        for score, piece, move in sorted_moves:
+            board.make_move(piece, move[0], move[1], True)
+            mini = minimax2(board, depth-1,True, alpha, beta)
+
+            enemy_data = mini[0][2]
+            evaluation = 0
+            if board.get_player_color() == "white":
+                evaluation -= board.evaluate(True)
+            else:
+                evaluation -= board.evaluate(False)
+
+            evaluation += enemy_data
+            if evaluation <= min_eval:
+                data.clear()
+                data.append([piece,move,evaluation,mini[0][0],mini[0][1]])
+            elif evaluation == min_eval:
+                data.append([piece,move,evaluation,mini[0][0],mini[0][1]])
+            min_eval = min(min_eval, evaluation)
+            positions_evaluated += 1
+            board.unmake_move(piece)
+            beta = min(beta, min_eval)
+            if beta <= alpha:
+                break
+    return data
 
 
 def get_ai_move(board):
@@ -101,7 +179,7 @@ def get_ai_move(board):
     data = minimax(board,4,True, -10000, 10000)
     random_move = 0
     #if len(data) > 1:
-        #random_move = random.randint(0,len(data)-1)
+     #   random_move = random.randint(0,len(data)-1)
       #  print(data[0][2])
      #   print(data[random_move][2])
     #if data[0][2] > data[random_move][2]:
@@ -117,11 +195,11 @@ def get_ai_move(board):
         row = abs(best_move[0]-8)
     end = time.perf_counter()
     time_taken = round(end-start,2)
-    print(f"Data: {data}")
-    print("Time: " + str(time_taken) + " seconds taken")
-    print(str(piece) + " moved to " + str(column) + "," + str(row))
-    print("Best move val: " + str(data[random_move][2]))
-    print("Positions evaluated: " + str(positions_evaluated))
+    #print(f"Data: {data}")
+    #print("Time: " + str(time_taken) + " seconds taken")
+    #print(str(piece) + " moved to " + str(column) + "," + str(row))
+    #print("Best move val: " + str(data[random_move][2]))
+    #print("Positions evaluated: " + str(positions_evaluated))
     positions_evaluated = 0
 
     if data[random_move][2] < -10000:
@@ -132,8 +210,10 @@ def get_ai_move(board):
         graphics.game_over_txt = 'BLACK WINS!'
     else:
         board.make_move(piece,best_move[0],best_move[1])
-    print(f"The computer thinks the best enemy move is {data[random_move][3]} moving to {data[random_move][4]}")
-    print(f"Best enemy move: {minimax(board,3,False, -10000, 10000)[0]}")
+    print(f"Computer best move: {data[random_move][3]} moving to {data[random_move][4]}")
+    print(f"Actual best move: {minimax(board,3,False, -10000, 10000)[0][:2]}")
+    #print(f"The board is the same: {board == data[random_move][5]}")
+    #print(data[random_move][5])
     turn += 1
 
 
